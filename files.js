18ce56fe77adf15a96e873bde47c9a97a9e61a72
binarymax <max_irwin@yahoo.com>
2022-03-13 16:17:02 -0400

    Clustered

--- files.js
+++ files.js
@@ -1,9 +1,8 @@
 import fs from "fs";
 
-function get_files() {
+export function get_files(min,max) {
     const files = [];
-    let k=0;
-    for(let i = 1; i <= 50; i++) {
+    for(let i = (min||1); i <= (max||50); i++) {
         if (i!=35) {
             let path = "parts/"+i+"/";
             let out = "vectors/"+i+"/";
@@ -23,9 +22,14 @@ function get_files() {
     return files;
 }
 
-export function batch(n) {
+export function total_files(min,max) {
+    return get_files(min,max).length;
+}
 
-    const files = get_files();
+
+export function batch(n,min_folder,max_folder) {
+
+    const files = get_files(min_folder,max_folder);
 
     let batches = Array.apply(null, Array(n)).map(()=>[]);
 
@@ -35,5 +39,19 @@ export function batch(n) {
     }
 
     return batches;
+
+}
+
+export function slice(threads,thread_num,min_folder,max_folder) {
+
+    const files = get_files(min_folder,max_folder);
+
+    let queue = [];
+
+    for(var i=thread_num;i<files.length;i+=threads) {
+        queue.push(files[i]);
+    }
+
+    return queue;
 
 }
---
+++ threaded.js
@@ -0,0 +1,92 @@
+import fs from "fs";
+import progress from "progress";
+import { fork } from "child_process";
+import { request } from "./request.js";
+import { batch,slice,total_files } from "./files.js";
+import { program } from "commander";
+
+program.option("-c, --child", false);
+program.option("-w, --workers <number>",2);
+program.option("-t, --thread <number>",0);
+program.option("-h, --host <string>","127.0.0.1");
+program.option("-u, --url <string>");
+program.parse();
+
+let workers = parseInt(program.opts().workers);
+let host = program.opts().host;
+
+if (!program.opts().child) {
+
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    let total = total_files(1,2);
+    var bar = new progress("Inferring [:bar] :percent remaining::etas elapsed::elapsed (:current/:total)", {complete: "=", incomplete: " ", width: 50, total: total});
+
+    let threads_completed = 0;
+    let errors = [];
+
+    for (let n = 0; n < workers; n++) {
+        (function(thread_num) {
+            const port = 5050+thread_num;
+            const url = `http://${host}:${port}/sentence-transformers`;
+            const child = fork("./threaded.js", ["--child","--thread",thread_num,"--workers",workers,"--url",url,"--host",host], { signal });
+            child.
+              on("message", (event) => {
+                
+                if (event.type=="error") {
+                    errors.push(event.data);
+                }
+
+                if (event.type=="done") {
+                    if(++threads_completed == workers) {
+                        console.log(`DONE! Total errors: ${errors.length}`);
+                    }
+                } else {
+                    bar.tick();
+                }
+              }).
+              on("error", (err) => {
+                console.log("*************** Unexpected Error! ***************");
+                console.log(err);
+              });
+        })(n);
+    }
+
+} else {
+
+    const url = program.opts().url;
+    const thread_num = parseInt(program.opts().thread);
+    const sliced = slice(workers,thread_num,1,2);
+    
+    console.log(`${thread_num} :: ${sliced.length}`);
+
+    for (var i=0;i<sliced.length;i++) {
+        let vectors = [];
+        let errors = [];
+        let file = sliced[i];
+
+        let part = JSON.parse(fs.readFileSync(file.filename,"utf-8"));
+        for (var j=0;j<part.fields.p.length;j++) {
+            let response = await request(url,part.fields.p[j]);
+            if (response[1]) {
+                vectors.push(response[1].outputs);
+            } else {
+                errors.push(response[0]);
+                vectors.push([]);                
+            }
+        }
+
+        if (errors.length==0) {
+            process.send({"type":"success","data":{"worker":thread_num,"file":file.filename}});
+        } else {
+            process.send({"type":"error","data":{"worker":thread_num,"file":file.filename}});
+        }
+
+        part.fields.vectors = vectors;
+        fs.writeFileSync(file.outfile,JSON.stringify(part,null,2),"utf-8");
+    }
+    process.send({"type":"done","data":{"worker":thread_num}});
+    process.exit(0);
+
+}
